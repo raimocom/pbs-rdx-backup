92754318666654549726f674945cafa2be6c3c38
root <root@proxmox.nbp.local>
2023-09-09 01:53:50 +0200

    added prepare_media, more checks for snapshot availability, code cleanup

--- prepare_backup_media.sh
+++ prepare_backup_media.sh
@@ -6,8 +6,10 @@ wipefs -a /dev/disk/by-id/$(cat backup_media.db | awk '{print $1}')
 
 #create zpool
 zpool create -o ashift=12 nbp-zfs-usb-backup /dev/disk/by-id/$(cat backup_media.db | awk '{print $1}')
+zfs set com.sun:auto-snapshot=false nbp-zfs-usb-backup
+echo $?
 
 #check if pool exists
-fdisk -l /dev/disk/by-id/$(cat backup_media.db | awk '{print $1}') | grep "Apple ZFS" >/dev/null
-if ($? == 0) then echo ZFS Partition found ; fi
+#fdisk -l /dev/disk/by-id/$(cat backup_media.db | awk '{print $1}') | grep "Apple ZFS" >/dev/null
+#if ($? == 0) then echo ZFS Partition found ; fi
 
