3494ae1e744e10a258d7499cf84499c030a6c7c3
raimocom <raimocom@users.noreply.github.com>
2024-03-27 03:12:13 +0100

    Create magic_sysrq_reboot.sh

---
+++ magic_sysrq_reboot.sh
@@ -0,0 +1,9 @@
+#emergency reboot in case of suspended ZPOOL  (simple reboot could hang the system indefinitely)
+qm stop 100
+qm stop 150
+pct stop 120
+sync
+sync
+sync
+echo 128 > /proc/sys/kernel/sysrq
+echo b > /proc/sysrq-trigger
